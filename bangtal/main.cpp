2a0a47a928906c3d757174e45a3b17870a19da95
dieu1024 <jwoo102405@gmail.com>
2021-09-25 00:38:09 +0900

    Initial Commit

---
+++ bangtal/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  bangtal
+//
+//  Created by 김지우 on 2021/09/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
